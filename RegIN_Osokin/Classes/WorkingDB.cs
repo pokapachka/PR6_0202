80b131a93c76343e5657406f37e5a2ac6933e25b
DanilGashev <lalalamistikc31@gmail.com>
2024-12-14 09:10:09 +0500

    Разработанный класс WorkingDB. Осокин

---
+++ RegIN_Osokin/Classes/WorkingDB.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegIN_Osokin.Classes
+{
+    public class WorkingDB
+    {
+        readonly static string connection = "server=localhost;port=3306;database=regin;user=root;pwd=;";
+        public static MySqlConnection OpenConnection()
+        {
+            try
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection(connection);
+                mySqlConnection.Open();
+                return mySqlConnection;
+            }
+            catch (Exception exp)
+            {
+                Debug.WriteLine(exp.Message);
+                return null;
+            }
+        }
+        public static MySqlDataReader Query(string Sql, MySqlConnection mySqlConnection)
+        {
+            MySqlCommand mySqlCommand = new MySqlCommand(Sql, mySqlConnection);
+            return mySqlCommand.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection mySqlConnection)
+        {
+            mySqlConnection.Close();
+            MySqlConnection.ClearPool(mySqlConnection);
+        }
+        public static bool OpenConnection(MySqlConnection mySqlConnection)
+        {
+            return mySqlConnection != null && mySqlConnection.State == System.Data.ConnectionState.Open;
+        }
+    }
+}
