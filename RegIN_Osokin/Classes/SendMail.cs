91c04b11bc24efb6027a947f28f4cce2af43ee2a
DanilGashev <lalalamistikc31@gmail.com>
2024-12-14 09:24:55 +0500

    Разработанный класс отправки сообщения на почту . Осокин

---
+++ RegIN_Osokin/Classes/SendMail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RegIN_Osokin.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                EnableSsl = true,
+            };
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект RegIn", Message);
+        }
+    }
+}
