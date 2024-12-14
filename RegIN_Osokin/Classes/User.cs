e1639b467ee3ac5935b00266fbfc2b772c6b5d2f
DanilGashev <lalalamistikc31@gmail.com>
2024-12-14 09:53:09 +0500

    не доделанный класс. Осокин

---
+++ RegIN_Osokin/Classes/User.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegIN_Osokin.Classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+        public DateTime DateUpdate { get; set; }
+        public DateTime DateCreate { get; set; }
+        public CorrectLogin HandlerCorrectLogin;
+        public InCorrectLogin HandlerInCorrectLogin;
+        public delegate void CorrectLogin();
+        public delegate void InCorrectLogin();
+        public void GetUserLogin(string Login)
+        {
+            this.Id = -1;
+            this.Login = String.Empty;
+            this.Password = String.Empty;
+            this.Name = String.Empty;
+            this.Image = new byte[0];
+            MySqlConnection mySqlConnection = WorkingDB.OpenConnection();
+            if (WorkingDB.OpenConnection(mySqlConnection))
+            {
+                MySqlDataReader userQuery = WorkingDB.Query($"Select * from 'users' where  'Login' = '{Login}'", mySqlConnection);
+                if (userQuery.HasRows)
+                {
+                    userQuery.Read();
+                    this.Id = userQuery.GetInt32(0);
+                    this.Login = userQuery.GetString(1);
+                    this.Password = userQuery.GetString(2);
+                    this.Name = userQuery.GetString(3);
+                    if (!userQuery.IsDBNull(4))
+                    {
+                        this.Image = new byte[64 * 1024];
+                        userQuery.GetBytes(4, 0, Image, 0, Image.Length);
+                    }
+                    this.DateUpdate = userQuery.GetDateTime(5);
+
+                }
+            }
+        }
+
+    }
+}
