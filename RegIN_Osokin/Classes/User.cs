6e8b8bc5c8ed62915f2bb58dfd97e6884f1dede7
pokapachka <pachkaosokin@yandex.ru>
2024-12-16 18:47:20 +0500

    Разработанный класс user. Осокин

--- RegIN_Osokin/Classes/User.cs
+++ RegIN_Osokin/Classes/User.cs
@@ -44,10 +44,77 @@ namespace RegIN_Osokin.Classes
                         userQuery.GetBytes(4, 0, Image, 0, Image.Length);
                     }
                     this.DateUpdate = userQuery.GetDateTime(5);
-
+                    this.DateCreate = userQuery.GetDateTime(6);
+                    HandlerCorrectLogin.Invoke();
+                }
+                else
+                {
+                    HandlerInCorrectLogin.Invoke();
                 }
             }
+            else
+            {
+                HandlerInCorrectLogin.Invoke();
+            }
+            WorkingDB.CloseConnection(mySqlConnection);
         }
-
+        public void SetUser()
+        {
+            MySqlConnection mySqlConnection = WorkingDB.OpenConnection();
+            if (WorkingDB.OpenConnection(mySqlConnection))
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand($"Insert into 'users'('Login', 'Password', 'Name', 'Image', 'DateUpdate', 'DateCreate',) values (@Login, @Password, @Name, @Image, @DateUpdate, @DateCreate)", mySqlConnection);
+                mySqlCommand.Parameters.AddWithValue("@Login", this.Login);
+                mySqlCommand.Parameters.AddWithValue("@Password", this.Password);
+                mySqlCommand.Parameters.AddWithValue("@Name", this.Name);
+                mySqlCommand.Parameters.AddWithValue("@Image", this.Image);
+                mySqlCommand.Parameters.AddWithValue("@DateUpdate", this.DateUpdate);
+                mySqlCommand.Parameters.AddWithValue("@DateCreate", this.DateCreate);
+                mySqlCommand.ExecuteNonQuery();
+            }
+            WorkingDB.CloseConnection(mySqlConnection);
+        }
+        public void CreateNewPassword()
+        {
+            if (Login != String.Empty)
+            {
+                Password = GeneratePass();
+                MySqlConnection mySqlConnection = WorkingDB.OpenConnection();
+                if (WorkingDB.OpenConnection(mySqlConnection))
+                {
+                    WorkingDB.Query($"update 'users' set 'Password'='{this.Password}' where 'Login'='{this.Login}'", mySqlConnection);
+                }
+                WorkingDB.CloseConnection(mySqlConnection);
+                SendMail.SendMessage($"Your account password has been changed.\nNew password: {this.Password}", this.Login);
+            }
+        }
+        public string GeneratePass()
+        {
+            List<Char> NewPassword = new List<Char>();
+            Random rnd = new Random();
+            char[] ArrNumbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] ArrSymbols = { '|', '-', '_', '!', '@', '#', '$', '%', '&', '*', '=', '+' };
+            char[] ArrUppercase = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', };
+            char[] ArrLowercase = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' };
+            for (int i = 0; i < 1; i++)
+                NewPassword.Add(ArrNumbers[rnd.Next(0, ArrNumbers.Length)]);
+            for (int i = 0; i < 1; i++)
+                NewPassword.Add(ArrSymbols[rnd.Next(0, ArrSymbols.Length)]);
+            for (int i = 0; i < 2; i++)
+                NewPassword.Add(char.ToUpper(ArrUppercase[rnd.Next(0, ArrUppercase.Length)]));
+            for (int i = 0; i < 6; i++)
+                NewPassword.Add(ArrLowercase[rnd.Next(0, ArrLowercase.Length)]);
+            for (int i = 0; i < NewPassword.Count; i++)
+            {
+                int RandomSymbol = rnd.Next(0, NewPassword.Count);
+                char Symbol = NewPassword[RandomSymbol];
+                NewPassword[RandomSymbol] = NewPassword[i];
+                NewPassword[i] = Symbol;
+            }
+            string NPassword = "";
+            for (int i = 0; i < NewPassword.Count; i++)
+                NPassword += NewPassword[i];
+            return NPassword;
+        }
     }
 }
