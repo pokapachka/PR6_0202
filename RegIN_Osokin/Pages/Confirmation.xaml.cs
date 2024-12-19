37b3b89b9d5f91cfe05deae2a2707092b937a661
student-a502 <student-a502@PC-A502-13>
2024-12-19 10:30:23 +0500

    Программирование окна Confirmation. Осокин

--- RegIN_Osokin/Pages/Confirmation.xaml.cs
+++ RegIN_Osokin/Pages/Confirmation.xaml.cs
@@ -20,9 +20,65 @@ namespace RegIN_Osokin.Pages
     /// </summary>
     public partial class Confirmation : Page
     {
+        public enum TypeConfirmation
+        {
+            Login,
+            Regin
+        }
+        TypeConfirmation ThisTypeConfirmation;
+        public int Code = 0;
         public Confirmation()
         {
             InitializeComponent();
+            ThisTypeConfirmation = TypeConfirmation;
+            SendMailCode();
         }
+        public void SendMailCode()
+        {
+            Code = new Random().Next(100000, 999999);
+            Classes.SendMail.SendMessage($"Login code: {Code}", MainWindow.mainWindow.UserLogIn.Login);
+            TimerSendMailCode();
+        }
+        public async void TimerSendMailCode()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                LTimer.Content = $"A second message can be sent after {60 - i} seconds";
+                await Task.Delay(1000);
+            }
+
+            BSendMessage.IsEnabled = true;
+            LTimer.Content = "";
+        }
+        private void SendMail(object sender, RoutedEventArgs e) => SendMailCode();
+        private void SetCode(object sender, KeyEventArgs e)
+        {
+            if (TbCode.Text.Length == 6)
+                SetCode();
+
+        }
+        private void SetCode(object sender, RoutedEventArgs e) => SetCode();
+        void SetCode()
+        {
+            if (TbCode.Text == Code.ToString() && TbCode.IsEnabled == true)
+            {
+                TbCode.IsEnabled = false;
+
+                string message = ThisTypeConfirmation == TypeConfirmation.Login
+                    ? "Авторизация пользователя успешно подтверждена."
+                    : "Регистрация пользователя успешно подтверждена.";
+                MessageBox.Show(message);
+
+                if (ThisTypeConfirmation == TypeConfirmation.Regin)
+                {
+                    MainWindow.mainWindow.UserLogIn.SetUser();
+                }
+                else
+                {
+                    MainWindow.mainWindow.OpenPage(new PinCode());
+                }
+            }
+        }
+        private void OpenLogin(object sender, MouseButtonEventArgs e) => MainWindow.mainWindow.OpenPage(new Login());
     }
 }
